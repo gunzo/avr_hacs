14999b9bb43e4d7b88564f5d888746e06f40cf40
Hannes Wallnöfer <h.wallnoefer@gmail.com>
2012-04-21 14:34:48 +0200

    Added timer 1 defs and funktions (Same as t0 has until now)

--- include/timers.h
+++ include/timers.h
@@ -221,3 +221,222 @@ void t0_stop()
  * @see T0_STOP
  */
 #define T0_RESET ( TCNT0 = 0x00 )
+
+
+/** 
+ * @brief Setting up timer1 in Clear Timer on Compare mode
+ *
+ * The Clear Timer on Compare mode will clear the timer when it has counted to 
+ * a certain value (Parameter TOP). This macro enables the interrupt for this
+ * event. The physical output pin for the timer 1A compare register, oc1a, 
+ * will be deactivated. This active behaviour might change in the future.
+ *
+ * @note Unlicke the other timers on the Atmega32, this timer is 16bits wide
+ * 	 and therefore supports higher resolutions than other timers.
+ *
+ * @param TOP 16 bit value at witch the timer will be cleared.
+ */
+#define T1_CTC_INT( TOP ) do{ \
+\
+	/* Makeing sure physical pin OC1A is not touched */\
+	/* Clearing COM1A0 and COM1A1 */\
+	TCCR1A &= ~(_BV( COM1A0 )|_BV( COM1A1 ));\
+\
+	/* Setup CTC mode in the TCCR0 register */\
+	TCCR1B |=   _BV( WGM12 );		/* Setting  WGM12       */\
+	TCCR1A &= ~(_BV( WGM11 )|_BV( WGM10 ));	/* Clearing WGM11, WGM10*/\
+	TCCR1B &= ~ _BV( WGM13 );		/* Clearing WGM13       */\
+\
+	/* Assigning TOP parameter to the Output Compare Register 1A */\
+	OCR1A = TOP;\
+\
+	/* Setting the Output Compare Match Interrupt Enable 1A bit, this */\
+	/* enables the interrupt when a compare match occurs. */\
+	TIMSK |= _BV( OCIE1A );\
+\
+	/* Setting the Global Interupt Enable bit, so that all interrupts will */\
+	/* work. */\
+	SREG |= _BV( 7 );\
+}while(0)
+
+
+/** 
+ * @brief Setting up timer1 in Clear Timer on Compare mode.
+ *
+ * This funktion uses the ::T1_CTC_INT( TOP ) definition as its body.
+ * Therfore the behavour is exacly the same.
+ *
+ * Example usage: t1_ctc_int(500)
+ *
+ * @param top 16 bit value at witch the timer will be cleared.
+ * @see T1_CTC_INT(TOP)
+ * @see t1_start(uint16_t clockdivision)
+ *
+ */
+void t1_ctc_int(uint16_t top)
+{
+	T1_CTC_INT( top );
+}
+
+
+/**
+ * @brief Deactivates CTC interupt for Timer 1
+ *
+ * The Clear Timer on Compare match interrupt will be dissabled for timer 1. If
+ * the interupt was not enabled, this definition will waste a single cycle.
+ *
+ * @see T1_CTC_INT( TOP )
+ * @see T1_CTC_INT_ON
+ * @see t1_ctc_int_off(void)
+ */
+#define T1_CTC_INT_OFF ( TIMSK &= ~_BV( OCIE1A ) )
+
+
+/**
+ * @brief Deactivates CTC interupt for Timer 1
+ *
+ * The Clear Timer on Compare match interrupt will be dissabled for timer 1. If
+ * the interupt was not enabled.
+ *
+ * This funktion uses the ::T1_CTC_INT_OFF as its Body.
+ *
+ * @see T1_CTC_INT_OFF
+ * @see t1_ctc_int(uint16_t top)
+ * @see t1_ctc_int_on(void)
+ */
+void t1_ctc_int_off(void)
+{
+	T1_CTC_INT_OFF;
+}
+
+
+/**
+ * @brief Activates CTC interupt for Timer 1
+ *
+ * The Clear Timer on Compare match interrupt will be Enabled for timer 1. If
+ * the interupt was not disabled, this definition will waste a single cycle.
+ *
+ * @see T1_CTC_INT( TOP )
+ * @see T1_CTC_INT_OFF
+ * @see t1_ctc_int_on(void)
+ */
+#define T1_CTC_INT_ON ( TIMSK |= _BV( OCIE1A ) ) 
+
+
+/**
+ * @brief Activates CTC interupt for Timer 1
+ *
+ * The Clear Timer on Compare match interrupt will be Enabled for timer 1. If
+ * the interupt was not disabled.
+ *
+ * This funktion uses the ::T1_CTC_INT_ON as its Body.
+ *
+ * @see T1_CTC_INT_ON
+ * @see T1_CTC_INT_OFF
+ * @see t1_ctc_int_on(void)
+ */
+void t1_ctc_int_on(void)
+{
+	T1_CTC_INT_ON;
+}
+
+
+/**
+ * @brief Starting timer1.
+ *
+ * The timer will be started with the given division factor. See parameter 
+ * \b CLOCKDIVISION.
+ *
+ * @param CLOCKDIVISION Dividing factor for the main clock.
+ *                      Values are: \b 1 (no division), \b 8, \b 64, \b 256 and
+ *                      \b 1024.
+ *                      If another value than these is given then no division 
+ *                      will be applied.
+ * @see T1_STOP
+ * @see t1_start(uint16_t clockdivision)
+ */
+#define T1_START( CLOCKDIVISION ) do{ \
+	/* Converting CLOCKDIVISION to a binary represation. */\
+	switch ( CLOCKDIVISION ) \
+	{ \
+		case 1:		TCCR1B |=  (_BV( CS10 )); /* Setting bits to 1*/ \
+				TCCR1B &= ~(_BV( CS12 )|_BV( CS11 )); /* Setting bits to 0*/ \
+				break; \
+		case 8:		TCCR1B |=   _BV( CS11 ); /* Setting bits to 1*/ \
+				TCCR1B &= ~(_BV( CS12 )|_BV( CS10 )); /* Setting bits to 0*/ \
+				break; \
+		case 64:	TCCR1B |=  (_BV( CS11 )|_BV( CS10 )); /* Setting bits to 1*/ \
+				TCCR1B &= ~(_BV( CS12 )); /* Setting bits to 0*/ \
+				break; \
+		case 256:	TCCR1B |=  (_BV( CS12 )); /* Setting bits to 1*/ \
+				TCCR1B &= ~(_BV( CS11 )|_BV( CS10 )); /* Setting bits to 0*/ \
+				break; \
+		case 1024:	TCCR1B |=  (_BV( CS12 )|_BV( CS10 )); /* Setting bits to 1*/ \
+				TCCR1B &= ~(_BV( CS11 )); /* Setting bits to 0*/ \
+				break; \
+		default:	/* Default setting is the same as 1, no division. */ \
+				TCCR1B |=  (_BV( CS10 )); /* Setting bits to 1*/ \
+				TCCR1B &= ~(_BV( CS12 )|_BV( CS11 )); /* Setting bits to 0*/ \
+				break; \
+	} \
+}while(0)
+
+
+/** 
+ * @brief Funktion for starting timer1.
+ *
+ * This funktion uses the ::T1_START( CLOCKDIVISION ) definition as its body.
+ * Therfore the behavour is exacly the same.
+ *
+ * Example usage: t1_start(256);
+ *
+ * @param clockdivision Dividing factor for the main clock.
+ *                      Values are: \b 1 (no division), \b 8, \b 64, \b 256 and
+ *                      \b 1024.
+ *                      If another value than these is given then no division 
+ *                      will be applied.
+ * @see T1_START( CLOCKDIVISION )
+ *
+ */
+void t1_start(uint16_t clockdivision)
+{
+	T1_START( clockdivision );
+}
+
+/**
+ * @brief Stopping timer1
+ *
+ * The timer will be stopped.
+ *
+ * @see T1_START( CLOCKDIVISION )
+ */
+#define T1_STOP (TCCR1B &= ~(_BV( CS12 )|_BV( CS11 )|_BV( CS10 )) )
+
+/**
+ * @brief Funktion for stopping timer1.
+ *
+ * This funktion uses the ::T1_STOP definition as its body. Therefore, the
+ * behaviour is exacly the same.
+ *
+ * @see T1_STOP
+ */
+void t1_stop()
+{
+	T1_STOP;
+}
+
+
+/**
+ * @brief Resets timer 1 count value
+ *
+ * This will clear the value in the count register for timer 1. The timer will
+ * count from 1 after this. 
+ *
+ * This can be done wherter the timer is running or not.
+ *
+ * @see T1_START( clockdivision )
+ * @see T1_STOP
+ */
+#define T1_RESET ( TCNT1 = 0x0000 )
+
+
